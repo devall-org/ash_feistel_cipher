d4054c4d6ca14717a488e2d6f797a393eb036353
Jechol Lee <jechol@devall.org>
2026-02-27 09:32:03 +0900

    Add time_offset DSL support and docs (#19)
    ## Summary
    - add time_offset to AshFeistelCipher DSL and transformer-generated
    migration options
    - extend tests/resources to verify custom time_offset is rendered in
    generated trigger SQL
    - document operational reason for time_offset for local business-day
    bucket boundary alignment

---
+++ test/support/custom_time_offset_resource.ex
@@ -0,0 +1,23 @@
+defmodule AshFeistelCipher.Test.CustomTimeOffsetResource do
+  @moduledoc false
+  use Ash.Resource,
+    domain: AshFeistelCipher.Test.Domain,
+    data_layer: AshPostgres.DataLayer,
+    extensions: [AshFeistelCipher]
+
+  postgres do
+    table "custom_time_offset_resources"
+    repo AshFeistelCipher.Test.Repo
+  end
+
+  attributes do
+    integer_sequence :seq
+
+    encrypted_integer :id,
+      from: :seq,
+      time_offset: 21_600,
+      primary_key?: true,
+      allow_nil?: false,
+      public?: true
+  end
+end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,6 +2,7 @@
 Code.require_file("support/verifier_helpers.ex", __DIR__)
 Code.require_file("support/repo.ex", __DIR__)
 Code.require_file("support/test_resources.ex", __DIR__)
+Code.require_file("support/custom_time_offset_resource.ex", __DIR__)
 Code.require_file("support/domain.ex", __DIR__)
 
 ExUnit.start()
