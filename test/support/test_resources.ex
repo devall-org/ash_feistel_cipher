c4ef2b69b7e8916a67d3920e1d3ce7aebee3ae9d
Jechol Lee <jechol@devall.org>
2026-02-27 09:28:11 +0900

    Remove formatter-only churn from test resources

--- test/support/test_resources.ex
+++ test/support/test_resources.ex
@@ -6,14 +6,14 @@ defmodule AshFeistelCipher.Test.ValidResource do
     extensions: [AshFeistelCipher]
 
   postgres do
-    table("valid_resources")
-    repo(AshFeistelCipher.Test.Repo)
+    table "valid_resources"
+    repo AshFeistelCipher.Test.Repo
   end
 
   attributes do
-    integer_sequence(:seq)
-    encrypted_integer(:id, from: :seq, primary_key?: true, allow_nil?: false, public?: true)
-    attribute(:name, :string)
+    integer_sequence :seq
+    encrypted_integer :id, from: :seq, primary_key?: true, allow_nil?: false, public?: true
+    attribute :name, :string
   end
 end
 
@@ -25,23 +25,22 @@ defmodule AshFeistelCipher.Test.MultipleEncryptsResource do
     extensions: [AshFeistelCipher]
 
   postgres do
-    table("multiple_encrypts")
-    repo(AshFeistelCipher.Test.Repo)
+    table "multiple_encrypts"
+    repo AshFeistelCipher.Test.Repo
   end
 
   attributes do
-    integer_sequence(:seq)
+    integer_sequence :seq
 
-    encrypted_integer(:id,
+    encrypted_integer :id,
       from: :seq,
       data_bits: 52,
       primary_key?: true,
       allow_nil?: false,
       public?: true
-    )
 
-    encrypted_integer(:referral_code, from: :seq, data_bits: 40, key: 12345)
-    attribute(:name, :string)
+    encrypted_integer :referral_code, from: :seq, data_bits: 40, key: 12345
+    attribute :name, :string
   end
 end
 
@@ -53,20 +52,19 @@ defmodule AshFeistelCipher.Test.CustomBitsResource do
     extensions: [AshFeistelCipher]
 
   postgres do
-    table("custom_bits_resources")
-    repo(AshFeistelCipher.Test.Repo)
+    table "custom_bits_resources"
+    repo AshFeistelCipher.Test.Repo
   end
 
   attributes do
-    integer_sequence(:seq)
+    integer_sequence :seq
 
-    encrypted_integer(:id,
+    encrypted_integer :id,
       from: :seq,
       data_bits: 40,
       primary_key?: true,
       allow_nil?: false,
       public?: true
-    )
   end
 end
 
@@ -78,20 +76,19 @@ defmodule AshFeistelCipher.Test.CustomKeyResource do
     extensions: [AshFeistelCipher]
 
   postgres do
-    table("custom_key_resources")
-    repo(AshFeistelCipher.Test.Repo)
+    table "custom_key_resources"
+    repo AshFeistelCipher.Test.Repo
   end
 
   attributes do
-    integer_sequence(:seq)
+    integer_sequence :seq
 
-    encrypted_integer(:id,
+    encrypted_integer :id,
       from: :seq,
       key: 999_888_777,
       primary_key?: true,
       allow_nil?: false,
       public?: true
-    )
   end
 end
 
@@ -103,13 +100,13 @@ defmodule AshFeistelCipher.Test.CustomSourceResource do
     extensions: [AshFeistelCipher]
 
   postgres do
-    table("custom_source_resources")
-    repo(AshFeistelCipher.Test.Repo)
+    table "custom_source_resources"
+    repo AshFeistelCipher.Test.Repo
   end
 
   attributes do
-    attribute(:sequence_number, :integer, primary_key?: true, allow_nil?: false, source: :seq_num)
-    encrypted_integer(:encrypted_id, from: :sequence_number, source: :enc_id)
+    attribute :sequence_number, :integer, primary_key?: true, allow_nil?: false, source: :seq_num
+    encrypted_integer :encrypted_id, from: :sequence_number, source: :enc_id
   end
 end
 
@@ -121,20 +118,19 @@ defmodule AshFeistelCipher.Test.CustomFunctionsPrefixResource do
     extensions: [AshFeistelCipher]
 
   postgres do
-    table("custom_prefix_resources")
-    repo(AshFeistelCipher.Test.Repo)
+    table "custom_prefix_resources"
+    repo AshFeistelCipher.Test.Repo
   end
 
   attributes do
-    integer_sequence(:seq)
+    integer_sequence :seq
 
-    encrypted_integer(:id,
+    encrypted_integer :id,
       from: :seq,
       functions_prefix: "crypto",
       primary_key?: true,
       allow_nil?: false,
       public?: true
-    )
   end
 end
 
@@ -146,14 +142,14 @@ defmodule AshFeistelCipher.Test.CustomSchemaResource do
     extensions: [AshFeistelCipher]
 
   postgres do
-    table("custom_schema_resources")
-    schema("accounts")
-    repo(AshFeistelCipher.Test.Repo)
+    table "custom_schema_resources"
+    schema "accounts"
+    repo AshFeistelCipher.Test.Repo
   end
 
   attributes do
-    integer_sequence(:seq)
-    encrypted_integer(:id, from: :seq, allow_nil?: false, primary_key?: true)
+    integer_sequence :seq
+    encrypted_integer :id, from: :seq, allow_nil?: false, primary_key?: true
   end
 end
 
@@ -165,45 +161,19 @@ defmodule AshFeistelCipher.Test.CustomRoundsResource do
     extensions: [AshFeistelCipher]
 
   postgres do
-    table("custom_rounds_resources")
-    repo(AshFeistelCipher.Test.Repo)
+    table "custom_rounds_resources"
+    repo AshFeistelCipher.Test.Repo
   end
 
   attributes do
-    integer_sequence(:seq)
+    integer_sequence :seq
 
-    encrypted_integer(:id,
+    encrypted_integer :id,
       from: :seq,
       rounds: 8,
       primary_key?: true,
       allow_nil?: false,
       public?: true
-    )
-  end
-end
-
-defmodule AshFeistelCipher.Test.CustomTimeOffsetResource do
-  @moduledoc false
-  use Ash.Resource,
-    domain: AshFeistelCipher.Test.Domain,
-    data_layer: AshPostgres.DataLayer,
-    extensions: [AshFeistelCipher]
-
-  postgres do
-    table("custom_time_offset_resources")
-    repo(AshFeistelCipher.Test.Repo)
-  end
-
-  attributes do
-    integer_sequence(:seq)
-
-    encrypted_integer(:id,
-      from: :seq,
-      time_offset: 21_600,
-      primary_key?: true,
-      allow_nil?: false,
-      public?: true
-    )
   end
 end
 
@@ -215,14 +185,14 @@ defmodule AshFeistelCipher.Test.PrimaryKeyResource do
     extensions: [AshFeistelCipher]
 
   postgres do
-    table("primary_key_resources")
-    repo(AshFeistelCipher.Test.Repo)
+    table "primary_key_resources"
+    repo AshFeistelCipher.Test.Repo
   end
 
   attributes do
-    integer_sequence(:seq)
-    encrypted_integer_primary_key(:id, from: :seq)
-    attribute(:name, :string)
+    integer_sequence :seq
+    encrypted_integer_primary_key :id, from: :seq
+    attribute :name, :string
   end
 end
 
@@ -234,13 +204,13 @@ defmodule AshFeistelCipher.Test.PrimaryKeyCustomOptionsResource do
     extensions: [AshFeistelCipher]
 
   postgres do
-    table("primary_key_custom_resources")
-    repo(AshFeistelCipher.Test.Repo)
+    table "primary_key_custom_resources"
+    repo AshFeistelCipher.Test.Repo
   end
 
   attributes do
-    integer_sequence(:seq)
-    encrypted_integer_primary_key(:id, from: :seq, data_bits: 40, key: 12345, rounds: 8)
-    attribute(:name, :string)
+    integer_sequence :seq
+    encrypted_integer_primary_key :id, from: :seq, data_bits: 40, key: 12345, rounds: 8
+    attribute :name, :string
   end
 end
